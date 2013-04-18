d85df8e44c2f478af67b2509d26ee871125a6dcf
Michael Hotan <michael.hotan@gmail.com>
2013-04-17 17:45:00 -0700

    Initial commit of android end for users

---
+++ DineOnUserApplication/src/uw/cse/dineon/user/ParseApplication.java
@@ -0,0 +1,31 @@
+package uw.cse.dineon.user;
+
+import uw.cse.dineon.shared.DineOnConstants;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Add your initialization code here
+		Parse.initialize(this, DineOnConstants.APPLICATION_ID, DineOnConstants.CLIENT_KEY);
+
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+	    
+		// If you would like all objects to be private by default, remove this line.
+		defaultACL.setPublicReadAccess(true);
+		
+		ParseACL.setDefaultACL(defaultACL, true);
+	}
+
+}
