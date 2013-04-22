b95185b145a38180246515d9a4dc8aa9885bb4b4
Michael Hotan <michael.hotan@gmail.com>
2013-04-22 12:54:12 -0700

    Created a new library project to contain shared resources for both projects.

---
+++ DineOnLibrary/src/uw/cse/dineon/library/DineOnConstants.java
@@ -0,0 +1,12 @@
+package uw.cse.dineon.library;
+
+/**
+ * Generalized wrapper class to hold constants that pertain to multiple modules with the applications
+ * @author mhotan
+ */
+public class DineOnConstants {
+
+	// Parse Related Cosntants to access Parse API
+	public static final String APPLICATION_ID = "10dCEpc2D8Rp7pC2uFaLCN7CtRcNSeZkz8d7eAaX";
+	public static final String CLIENT_KEY = "4NAn0437HLZpDMa5v0gS6JeYT28Q0vmxW19qWdSw";
+}
