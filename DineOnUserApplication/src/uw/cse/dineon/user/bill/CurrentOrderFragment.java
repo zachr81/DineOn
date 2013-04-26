2a5b225c31dafb1d88f0a59fa865b051b64b0cb6
Michael Hotan <michael.hotan@gmail.com>
2013-04-26 15:12:04 -0700

    TEST Commit:
    Created a new branch locally. From a local up to date copy of the master
    remote branch. Then added new class for Bill Fragment
    
    Removed Icon

---
+++ DineOnUserApplication/src/uw/cse/dineon/user/bill/CurrentOrderFragment.java
@@ -0,0 +1,11 @@
+package uw.cse.dineon.user.bill;
+
+import android.support.v4.app.Fragment;
+
+public class CurrentOrderFragment extends Fragment {
+
+	public CurrentOrderFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
