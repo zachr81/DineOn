67bf619725221b926f3caff70c52af0720bd7a9f
Michael Hotan <michael.hotan@gmail.com>
2013-04-26 13:16:45 -0700

    Test Change

--- DineOnLibrary/src/uw/cse/dineon/library/DineOnConstants.java
+++ DineOnLibrary/src/uw/cse/dineon/library/DineOnConstants.java
@@ -9,4 +9,10 @@ public class DineOnConstants {
 	// Parse Related Cosntants to access Parse API
 	public static final String APPLICATION_ID = "10dCEpc2D8Rp7pC2uFaLCN7CtRcNSeZkz8d7eAaX";
 	public static final String CLIENT_KEY = "4NAn0437HLZpDMa5v0gS6JeYT28Q0vmxW19qWdSw";
+	
+	// Request Code for multiple Activity use
+	public final static int REQUEST_CHECK_IN = 0x1;
+
+	// some change
+	
 }
