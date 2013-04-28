55ebc7b8d9b8e3f64e94ed8b257059656914193a
unknown <jordanmcneal22@gmail.com>
2013-04-27 18:47:44 -0700

    abstract class storable

---
+++ DineOnLibrary/src/uw/cse/dineon/library/Storable.java
@@ -0,0 +1,59 @@
+package uw.cse.dineon.library;
+
+import android.os.Bundle;
+
+import com.parse.ParseObject;
+
+/**
+ * The Parse cloud uses ParseObjects to store information in 
+ * the database. For any java object that we want to store
+ * in the cloud, there needs to be a convenient way to
+ * convert between ParseObjects and java objects.
+ * 
+ * This abstract class provides the method definitions that are
+ * required to convert ParseObjects and java objects.
+ * 
+ * @author Jordan
+ *
+ */
+public abstract class Storable {
+	
+	
+	private String objId;
+	
+	/**
+	 * Inserts this objects fields and returns the Bundle
+	 * representation.
+	 * @return Bundle that represents this.
+	 */
+	public abstract Bundle bundle(); 
+	
+	/**
+	 * Takes Bundle and sets the appropriate fields of the
+	 * instance of storable 
+	 * @param b Bundle that you want to convert to a Storable
+	 */
+	public abstract void unbundle(Bundle b);
+	
+	/**
+	 * Inserts this objects fields and returns the ParseObject
+	 * representation.
+	 * @return ParseObject that represents this.
+	 */
+	public  abstract ParseObject packObject();
+	
+	/**
+	 * Takes ParseObject and sets the appropriate fields of the
+	 * instance of storable 
+	 * @param pobj ParseObject that you want to convert to a Storable
+	 */
+	public abstract void unpackObject(ParseObject pobj);
+
+	public String getObjId() {
+		return objId;
+	}
+
+	public void setObjId(String objId) {
+		this.objId = objId;
+	}
+}
