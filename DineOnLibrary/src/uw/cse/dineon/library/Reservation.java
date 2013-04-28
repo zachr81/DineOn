f845430fea7376c23c60e1ec5164d3d33a8bc75a
Espeo196 <Espeo196@Espeo196-PC>
2013-04-27 19:19:43 -0700

    Initial skeleton implementations

---
+++ DineOnLibrary/src/uw/cse/dineon/library/Reservation.java
@@ -0,0 +1,88 @@
+package uw.cse.dineon.library;
+
+import com.parse.ParseObject;
+
+import android.os.Bundle;
+import android.text.format.Time;
+
+/**
+ * @author Espeo196
+ *
+ */
+public class Reservation extends Storable {
+	private UserInfo userInfo;
+	private RestaurantInfo restInfo;
+	private Time startTime;
+	private DiningSession currSession;
+
+	/**
+	 *
+	 */
+	public Reservation() {
+		// TODO
+	}
+
+	/**
+	 *
+	 * @return UserInfo
+	 */
+	public UserInfo getUserInfo() {
+		return userInfo;
+	}
+
+	/**
+	 *
+	 * @param info UserInfo
+	 */
+	public void setUserInfo(UserInfo info) {
+		this.userInfo = info;
+	}
+
+	/**
+	 *
+	 * @return RestaurantInfo
+	 */
+	public RestaurantInfo getRestInfo() {
+		return restInfo;
+	}
+
+	/**
+	 *
+	 * @param info RestaurantInfo
+	 */
+	public void setRestInfo(RestaurantInfo info) {
+		this.restInfo = info;
+	}
+
+	/**
+	 *
+	 * @return Time
+	 */
+	public Time getStartTime() {
+		return startTime;
+	}
+
+	/**
+	 *
+	 * @param start Time
+	 */
+	public void setStartTime(Time start) {
+		this.startTime = start;
+	}
+
+	/**
+	 *
+	 * @return DiningSession
+	 */
+	public DiningSession getCurrSession() {
+		return currSession;
+	}
+
+	/**
+	 *
+	 * @param currSess DiningSession
+	 */
+	public void setCurrSession(DiningSession currSess) {
+		this.currSession = currSess;
+	}
+}
