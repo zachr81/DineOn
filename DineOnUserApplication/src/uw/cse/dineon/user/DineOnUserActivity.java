9e8a825ed360bd520609afb875adc867013f77a1
espeo196 <espeo196@uw.edu>
2013-04-30 12:26:15 -0700

    Made a few changes to the styles and implemented onSaveInstanceState and OnRestore Instance State.

--- DineOnUserApplication/src/uw/cse/dineon/user/DineOnUserActivity.java
+++ DineOnUserApplication/src/uw/cse/dineon/user/DineOnUserActivity.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import uw.cse.dineon.library.DiningSession;
+import uw.cse.dineon.library.Order;
 import uw.cse.dineon.library.Storable;
 import uw.cse.dineon.library.util.DineOnConstants;
 import uw.cse.dineon.library.util.ParseUtil;
@@ -25,10 +26,10 @@ import android.view.MenuItem;
 
 /**
  * Generl Fragment Activity class that pertains to a specific user.
- * Once the User is logged in then they are allowed specific access to 
+ * Once the User is logged in then they are allowed specific access to
  * different regions of the application.
- * 
- * <b><b>In Particular their user specific preferences
+ *
+ * In Particular their user specific preferences
  * @author mhotan
  */
 public class DineOnUserActivity extends FragmentActivity {
@@ -36,19 +37,19 @@ public class DineOnUserActivity extends FragmentActivity {
 	private static final String TAG = DineOnUserActivity.class.getSimpleName();
 
 	protected DiningSession mDiningSession;
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
-		// Note that override this method does not mean the actualy 
+		// Note that override this method does not mean the actualy
 		//  UI Menu is updated this is done manually
 		//  See basic_menu under res/menu for ids
 		inflater.inflate(R.menu.basic_menu, menu);
-		
+
 		MenuItem item = menu.findItem(R.id.option_view_bill);
 		item.setEnabled(false);
 		item.setVisible(false);
-		
+
 		return true;
 	}
 
@@ -72,36 +73,37 @@ public class DineOnUserActivity extends FragmentActivity {
 			startActivityForResult(i, DineOnConstants.REQUEST_VIEW_CURRENT_ORDER);
 			break;
 		}
-		if (i != null)
+		if (i != null) {
 			startActivity(i);
+		}
 		return true;
 	}
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		QRCheckin.QRResult(requestCode, resultCode, intent);
 	}
 
 	/**
-	 * This is the defined call back method for when the 
+	 * This is the defined call back method for when the
 	 * User attempts to check in to a restaurant.  Once the check
 	 * @param session is a mapping from the arguments returned from the call
 	 * 	DineOnConstants.OBJ_ID => Parse Object ID for Dining Session
 	 */
-	public void onCheckInCallback(Map<String, String> session){
+	public void onCheckInCallback(Map<String, String> session) {
 		try {
 			// Extract the object ID from the return map
 			String objID = session.get(DineOnConstants.OBJ_ID);
-			
+
 			// Use Utility to call Parse and get the Dining Session instance
-			if (objID == null || objID.isEmpty()){
+			if (objID == null || objID.isEmpty()) {
 				// TODO Update the UI
-				// Handle the fail case where no dining session 
+				// Handle the fail case where no dining session
 				// was created
-			}			
+			}
 			
 			List<Storable> list = new ArrayList<Storable>();
-			// Then Bundle the Dining Session Instance into 
+			// Then Bundle the Dining Session Instance into
 			Method m = null;
 
 			m = DineOnUserActivity.class.getMethod("onDiningSessionRecievedCallback",
@@ -116,44 +118,118 @@ public class DineOnUserActivity extends FragmentActivity {
 		}
 
 	}
-	
+
 	/**
 	 * This is a callback for when the Dining Session is recieved via local
-	 * caching or 
-	 * @param list
+	 * caching or.
+	 * @param list List<Storable>
 	 */
-	public void onDiningSessionRecievedCallback(List<Storable> list){
+	public void onDiningSessionRecievedCallback(List<Storable> list) {
 		// Assert that the first item in the list is
 		// is a DiningSession
-		if (list != null && list.size() == 1){
+		if (list != null && list.size() == 1) {
 			throw new IllegalArgumentException("List returned is not valid: " + list);
 		}
-		mDiningSession = (DiningSession)list.get(0);
+		mDiningSession = (DiningSession) list.get(0);
 		// TODO Extract channel for push
 		// TODO Register for the channel and start listening for updates
 		// TODO Extract object id for restaurant
 
 		// Bundle up dining session
-		// Start RestaurantMainActivity with bundle 
+		// Start RestaurantMainActivity with bundle
 	}
-	
+
+	/**
+	 * Saves the instance of the current DiningSession. Information can
+	 * be recovered by using onRestoreInstanceState.
+	 *
+	 * @param savedInstanceState Bundle to store the current
+	 * 		activity's data to.
+	 */
 	@Override
 	public void onSaveInstanceState(Bundle savedInstanceState) {
+
+		// Save the fields of mDiningSession to savedInstanceState
+		savedInstanceState.putInt("sessToken", mDiningSession.getSessToken());
+		savedInstanceState.putInt("tableID", mDiningSession.getTableID());
+
+		List<Order> orderList = mDiningSession.getOrders();
+		int i = 1;
+		
+		// save number of orders to reclaim later
+		savedInstanceState.putInt("numOrders", orderList.size());
+		// save each order item, keep track individual orderswith concatenated ints
+		for (Order order : orderList) {
+			savedInstanceState.putInt("tableID_" + i, order.getTableID());
+			savedInstanceState.putInt("userID_" + i, order.getUserID());
+			savedInstanceState.putInt("restID_" + i, order.getRestID());
+			savedInstanceState.putInt("timestamp_" + i, order.getTimestamp());
+			
+			// use this long syntax to ensure that the correct type of MenuItem is used
+			List<uw.cse.dineon.library.MenuItem> itemList = order.getMenuItems();
+			int j = 1;
+			savedInstanceState.putInt("numItems_" + i, itemList.size());
+			// save each menu item in the order 
+			// fields saved as <fieldName>_<orderNumber>_<itemNumber>
+			for (uw.cse.dineon.library.MenuItem item : itemList) {
+				savedInstanceState.putInt("productID_" + i + "" + j, item.getProductID());
+				savedInstanceState.putDouble("price_" + i + "" + j, item.getPrice());
+				savedInstanceState.putString("description_" + i + "" + j, item.getDescription());
+				j++;
+			}
+			i++;
+		}
 		super.onSaveInstanceState(savedInstanceState);
-		// TODO Save state of all the fields of this activity
-		// mDiningSession
-		// Save to Parse then reference later
-	
 	}
-	
+
+	/**
+	 * Restores an instance of a DiningSession from the given Bundle
+	 * parameter.
+	 *
+	 * @param savedInstanceState Bundle that holds session information
+	 * 		to be restored.
+	 */
 	@Override
 	public void onRestoreInstanceState(Bundle savedInstanceState) {
-	  super.onRestoreInstanceState(savedInstanceState);
-	  // TODO Restore data
-	  // mDiningSession
+		super.onRestoreInstanceState(savedInstanceState);
+
+		if(savedInstanceState != null) {
+			mDiningSession.setSessToken(savedInstanceState.getInt("sessToken"));
+			mDiningSession.setTableID(savedInstanceState.getInt("tableID"));
+			int orderSize = savedInstanceState.getInt("numOrders");
+			List<Order> orderList = new ArrayList<Order>();
+		
+			// restore orders
+			for (int i = 1; i <= orderSize; i++) {
+				int itemSize = savedInstanceState.getInt("numItems_" + i);
+				List<uw.cse.dineon.library.MenuItem> itemList = 
+						  	new ArrayList<uw.cse.dineon.library.MenuItem>();
+				  
+				// grab fields for an order
+				int tableID = mDiningSession.getTableID();
+				int userID = savedInstanceState.getInt("userID_" + i);
+				int restID = savedInstanceState.getInt("restID_" + i);
+				int timestamp = savedInstanceState.getInt("timestamp_" + i);
+				Order order = new Order(tableID, userID, restID, timestamp, null);
+	
+				// add menu items to order before adding to mDiningSession
+				for (int j = 1; j <= itemSize; j++) {
+					// grab fields for a MenuItem
+					int productID = savedInstanceState.getInt("productID_" + i + "" + j);
+					double price = savedInstanceState.getInt("price_" + i + "" + j);
+					String description = savedInstanceState.getString("description_" + i + "" + j);
+	
+					// create new item with stored fields (see above) and add it
+					uw.cse.dineon.library.MenuItem item = 
+							new uw.cse.dineon.library.MenuItem(productID, price, description);
+					  
+					itemList.add(item);
+				}
+				// set MenuItems for current order and add the order to the session's list
+				order.setMenuItems(itemList);
+				orderList.add(order);
+			}
+			mDiningSession.setOrders(orderList);
+		} // TODO else set session to default?
 	}
-	
-	
-	
-	
 }
