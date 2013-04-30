d77ed22073015a722502bf8deff9692d3ef6c837
Michael Hotan <michael.hotan@gmail.com>
2013-04-30 13:16:22 -0700

    Updated the library manifest, added Utility function to check network connection

--- DineOnLibrary/src/uw/cse/dineon/library/util/Utility.java
+++ DineOnLibrary/src/uw/cse/dineon/library/util/Utility.java
@@ -2,6 +2,8 @@ package uw.cse.dineon.library.util;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 /**
  * Utility class to hold a lot of reusable method and procedures
@@ -11,18 +13,39 @@ import android.content.res.Configuration;
 public class Utility {
 
 	/**
-	 * Returns whether the screen can 
-	 * @param context
-	 * @return
+	 * Returns whether the screen can be split horizontally into two panes
+	 * for optimal user interaction.  Currently the restriction is 
+	 * that if the screen is very large 
+	 * <b>Currently: XLarge screen layout
+	 * <b><b>OR if the screen is large and in landscape
+	 * @param context Context of the activity
+	 * @return true if it is able to split, false otherwise
 	 */
 	public static boolean isPaneSplitable(Context context){
 		int mask = (context.getResources().getConfiguration().screenLayout & 
-			    Configuration.SCREENLAYOUT_SIZE_MASK);
+				Configuration.SCREENLAYOUT_SIZE_MASK);
 		boolean isLargeEnough = mask == Configuration.SCREENLAYOUT_SIZE_LARGE 
 				|| mask == Configuration.SCREENLAYOUT_SIZE_XLARGE;
-		boolean isLayoutOriented = context.getResources().getConfiguration().orientation
-				== Configuration.ORIENTATION_LANDSCAPE;
-		return isLargeEnough && isLayoutOriented;
+		//		boolean isLayoutOriented = context.getResources().getConfiguration().orientation
+		//				== Configuration.ORIENTATION_LANDSCAPE;
+		return isLargeEnough;
 	}
-	
+
+	/**
+	 * Checks whether the given context is connected to 
+	 * the network either via wifi or data
+	 * @param context Context that needs network
+	 * @return true if connected to the network, false otherwise
+	 */
+	public static boolean isConnectedToNetwork(Context context){
+		ConnectivityManager connMgr = (ConnectivityManager) 
+				context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
