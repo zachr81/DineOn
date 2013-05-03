ab972605a662762d7bd7197395c6f6a7551abc4f
Michael Hotan <michael.hotan@gmail.com>
2013-05-03 02:56:26 -0700

    Added Credential Validator to check email and passwords

--- DineOnLibrary/src/uw/cse/dineon/library/util/CredentialValidator.java
+++ DineOnLibrary/src/uw/cse/dineon/library/util/CredentialValidator.java
@@ -1,30 +1,89 @@
 package uw.cse.dineon.library.util;
 
+import java.util.regex.Pattern;
+
 /**
- * Static tool to help validate passwords.
+ * Static tool to help validate login credential syntax.
  * @author mhotan
  */
 public final class CredentialValidator {
 
-	private static final String RESOLVED = "Valid password";
-	
+	private static final String RESOLVED = "Valid!";
+
 	private static final int MIN_LENGTH = 1; // TODO change later
-	
+
 	private static final Resolution RESOLVED_INSTANCE = new Resolution();
-	private static final Resolution NULL_PW = new Resolution(false, "Can't have a NULL password");
+	private static final Resolution NULL_PW = new Resolution(false, "Can't have a null password");
 	private static final Resolution EMPTY_PW = new Resolution(false, 
 			"Can't have a password with no characters");
 	private static final Resolution SHORT_PW = new Resolution(false, 
 			"Password must have more then " + MIN_LENGTH + " characters");
-	
-	
+	private static final Resolution SPACEIN_PW = new Resolution(false, 
+			"Password can't contain spaces");
+	//TODO etc..
+
+
+	private static final Resolution NULL_UN = new Resolution(false, "Can't have a null username");
+	private static final Resolution EMPTY_UN = new Resolution(false, 
+			"Can't have a username with no characters");
+	private static final Resolution SHORT_UN = new Resolution(false,
+			"Username is to short");
+	private static final Resolution SPACEIN_UN = new Resolution(false, 
+			"Username can't contain spaces");
+	//TODO etc..
+
+	private static final Resolution INVALID_EMAIL = new Resolution(false, "Invalid email"); 
+
+	private static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
+			"[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" 
+					+ "\\@"
+					+ "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" 
+					+  "("
+					+ "\\."
+					+ "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}"
+					+ ")+" );
+
 	/**
 	 * Can't create instance.
 	 */
 	private CredentialValidator() { }
-	
+
 	/**
-	 * A Password validator that checks whether a string password would make a good password.
+	 * Syntactically validates email.
+	 * @param email email to validate
+	 * @return Resolution to return.
+	 */
+	public static Resolution isValidEmail(String email) {
+		if (EMAIL_ADDRESS_PATTERN.matcher(email).matches()) {
+			return RESOLVED_INSTANCE;
+		}
+		return INVALID_EMAIL;
+	}
+
+	/**
+	 * Syntactic username validator.
+	 * @param username usrename to validate
+	 * @return Resolution of this username
+	 */
+	public static Resolution isValidUserName(String username) {
+		if (username == null) {
+			return NULL_UN;
+		} 
+		if (username.isEmpty()) {
+			return EMPTY_UN;
+		}
+		if (username.length() < MIN_LENGTH) {
+			return SHORT_UN;
+		}
+		if (username.contains(" ")) {
+			return SPACEIN_UN;
+		}
+
+		return RESOLVED_INSTANCE;
+	}
+
+	/**
+	 * Syntactically validates passwords.
 	 * 
 	 * @param password Password to validate as a "Good" password
 	 * @return A resolution instance stating the result of validifying the password 
@@ -39,37 +98,46 @@ public final class CredentialValidator {
 		if (password.length() < MIN_LENGTH) {
 			return SHORT_PW;
 		}
-		
+		if (password.contains(" ")) {
+			return SPACEIN_PW;
+		}
+
 		return RESOLVED_INSTANCE;
 	}
-	
+
 	/**
 	 * TODO Might use later pending how we create messages
+	 * MH To lazy right now ;-)
 	 * Handles the distribution and creation of Resolution instances.
 	 * Allows memory conservation and virtualization of Resolution construction
 	 * @author mhotan
 	 */
 	private static class ResolutionFactory {
-//		
-//		/**
-//		 * 
-//		 * @param pw
-//		 * @return
-//		 */
-//		static Resolution getNullOrEmptyResolution(String pw){
-//			return NULL_PW;
-//		}
+		//		
+		//		/**
+		//		 * 
+		//		 * @param pw
+		//		 * @return
+		//		 */
+		//		static Resolution getNullOrEmptyResolution(String pw){
+		//			return NULL_PW;
+		//		}
 	}
-	
+
 	/**
+	 * General resolution class the has the ability to communicate
+	 * to the client whether the given request produced a valid result.
+	 * 
+	 * To see associated messages use getMessage().
+	 * To see if valid use isValid().
+	 * 
 	 * @author mhotan
-	 *
 	 */
 	public static class Resolution {
-		
+
 		private final boolean isResolved;
 		private final String message;
-		
+
 		/**
 		 * Returns resolved password result.
 		 * Only Password validator can generate a resolution
@@ -77,7 +145,7 @@ public final class CredentialValidator {
 		Resolution() {
 			this(true, RESOLVED);
 		}
-		
+
 		/**
 		 * 
 		 * @param valid true if valid password, false other wise
@@ -87,7 +155,7 @@ public final class CredentialValidator {
 			this.isResolved = valid;
 			this.message = message;
 		}
-		
+
 		/**
 		 * Returns whether the password is valid.
 		 * @return true if valid
@@ -95,7 +163,7 @@ public final class CredentialValidator {
 		public boolean isValid(){
 			return isResolved;
 		}
-		
+
 		/**
 		 * Returns the message associated with this resolution.
 		 * @return message as string literal
@@ -104,5 +172,5 @@ public final class CredentialValidator {
 			return message;
 		}
 	}
-	
+
 }
