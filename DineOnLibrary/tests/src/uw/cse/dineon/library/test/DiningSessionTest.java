af32f57fc636547b6fae4129d1e3a4282f016308
Mark Rathjen <mrathjen.uw@gmail.com>
2013-05-10 17:28:23 -0700

    Added the test projects

---
+++ DineOnLibrary/tests/src/uw/cse/dineon/library/test/DiningSessionTest.java
@@ -0,0 +1,75 @@
+package uw.cse.dineon.library.test;
+
+import junit.framework.TestCase;
+
+public class DiningSessionTest extends TestCase {
+
+	public DiningSessionTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testPackObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testDiningSessionInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testDiningSessionIntDate() {
+		fail("Not yet implemented");
+	}
+
+	public void testDiningSessionParseObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetPendingOrders() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCompletedOrders() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddPendingOrder() {
+		fail("Not yet implemented");
+	}
+
+	public void testOrderServed() {
+		fail("Not yet implemented");
+	}
+
+	public void testPaidOrder() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetTableID() {
+		fail("Not yet implemented");
+	}
+
+	public void testResetTableID() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetUsers() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetStartTime() {
+		fail("Not yet implemented");
+	}
+
+}
