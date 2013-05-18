2d31cae777169df7a85653d3c3e1cbb493a766c0
Garrett Lee <garrettlee23@gmail.com>
2013-05-18 08:39:01 -0700

    Commented out test

--- DineOnRestaurantApplication/tests/src/uw/cse/dineon/restaurant/test/RestaurantMainActivityTest.java
+++ DineOnRestaurantApplication/tests/src/uw/cse/dineon/restaurant/test/RestaurantMainActivityTest.java
@@ -13,10 +13,15 @@ import uw.cse.dineon.library.Restaurant;
 import uw.cse.dineon.library.RestaurantInfo;
 import uw.cse.dineon.library.UserInfo;
 import uw.cse.dineon.library.util.DineOnConstants;
+import uw.cse.dineon.restaurant.active.OrderDetailActivity;
+import uw.cse.dineon.restaurant.active.RequestDetailActivity;
 import uw.cse.dineon.restaurant.active.RestauarantMainActivity;
-import android.app.Activity;
+import android.app.Instrumentation.ActivityMonitor;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.view.KeyEvent;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -27,12 +32,16 @@ import com.parse.ParseUser;
 public class RestaurantMainActivityTest extends
 ActivityInstrumentationTestCase2<RestauarantMainActivity> {
 
-	private Activity mActivity;
+	private RestauarantMainActivity mActivity;
 	private Restaurant r;
 
 	private ParseUser mUser;
 
 	private UserInfo mUI;
+
+	private RestaurantInfo mRI;
+
+	int orderNum = 100;
 
 	private static String menuItemText = "Fake Menu Item 1"; 
 	private static String menuItemDescription = "Fake Menu Item 1 Description";
@@ -49,13 +58,18 @@ ActivityInstrumentationTestCase2<RestauarantMainActivity> {
 		mUser = ParseUser.logIn("glee23", "p");
 
 		mUI = new UserInfo(mUser);
-
+		mRI = new RestaurantInfo(mUser);
 		r = createFakeRestaurant(mUser);
+
+		ArrayList<Parcelable> dSessions = new ArrayList<Parcelable>();
+		DiningSession testSession = createFakeDiningSession(mUser);
+		dSessions.add(testSession);
 
 		Intent intent = new Intent(getInstrumentation().getTargetContext(),
 				RestauarantMainActivity.class);
 		intent.putExtra(DineOnConstants.KEY_USER, new DineOnUser(mUser));
 		intent.putExtra(DineOnConstants.KEY_RESTAURANT, r);
+		intent.putExtra(DineOnConstants.DINING_SESSION, dSessions);
 		setActivityIntent(intent);
 
 		setActivityInitialTouchMode(false);
@@ -72,7 +86,7 @@ ActivityInstrumentationTestCase2<RestauarantMainActivity> {
 	}
 
 	private Order createFakeOrder(ParseUser user){
-		return new Order(1, mUI, createFakeMenuItems());
+		return new Order(orderNum++, mUI, createFakeMenuItems());
 	}
 
 	private List<MenuItem> createFakeMenuItems(){
@@ -89,11 +103,26 @@ ActivityInstrumentationTestCase2<RestauarantMainActivity> {
 	private DiningSession createFakeDiningSession(ParseUser user) throws ParseException {
 		return new DiningSession(1, 
 				new GregorianCalendar().getTime(), 
-				mUI, new RestaurantInfo(user));
+				mUI, mRI);
 	}
 
 	protected void tearDown() throws Exception {
+		mUser = null;
+		mUI = null;
+		mActivity.finish();
 		super.tearDown();
+	}
+
+	public void testNumOrders() {
+		assertEquals(1, mActivity.getCurrentOrders().size());
+	}
+
+	public void testNumRequests() {
+		assertEquals(1, mActivity.getCurrentRequests().size());
+	}
+
+	public void testNumDiningSessions() {
+		assertEquals(1, mActivity.getCurrentSessions().size());
 	}
 
 	public void testOrderLayoutItemsPopulate() {
@@ -109,21 +138,31 @@ ActivityInstrumentationTestCase2<RestauarantMainActivity> {
 		TextView requestTitle = (TextView) mActivity.findViewById(uw.cse.dineon.restaurant.R.id.label_request_title);
 		TextView requestTime = (TextView) mActivity.findViewById(uw.cse.dineon.restaurant.R.id.label_request_time);
 		ImageButton arrowButton = (ImageButton) mActivity.findViewById(uw.cse.dineon.restaurant.R.id.button_expand_request);
-		System.out.println(requestTitle.getText().toString());
 		assertNotNull(requestTitle);
 		assertNotNull(requestTime);
 		assertNotNull(arrowButton);
 	}
-	
-//  Uncomment -- Will pass once onOrderRequestDetail is implemented
-//	public void testGoToOrderDetailActivity() {
-//		ActivityMonitor monitor = getInstrumentation().addMonitor(OrderDetailActivity.class.getName(), null, false);
-//		ImageButton orderArrowButton = (ImageButton) mActivity.findViewById(uw.cse.dineon.restaurant.R.id.button_expand_order);
-//		TouchUtils.clickView(this, orderArrowButton);
-//		TouchUtils.clickView(this, orderArrowButton);
-//		OrderDetailActivity startedActivity = (OrderDetailActivity) monitor.waitForActivityWithTimeout(2000);
+
+//	  Uncomment -- Will pass once onRequestRequestDetail is implemented
+//	public void testLaunchRequestDetailActivity() {
+//		ActivityMonitor monitor = getInstrumentation().addMonitor(RequestDetailActivity.class.getName(), null, false);
+//		List<CustomerRequest> requests = mActivity.getCurrentRequests();
+//        mActivity.onRequestRequestDetail(requests.get(0));
+//		RequestDetailActivity startedActivity = (RequestDetailActivity) monitor.waitForActivityWithTimeout(2000);
 //		assertNotNull(startedActivity);
-//		this.sendKeys(KeyEvent.KEYCODE_BACK);
+//		startedActivity.finish();
 //	}
+
+//	  Uncomment -- Will pass once onOrderRequestDetail is implemented
+//		public void testGoToOrderDetailActivity() {
+//			ActivityMonitor monitor = getInstrumentation().addMonitor(OrderDetailActivity.class.getName(), null, false);
+//			ImageButton orderArrowButton = (ImageButton) mActivity.findViewById(uw.cse.dineon.restaurant.R.id.button_expand_order);
+//			TouchUtils.clickView(this, orderArrowButton);
+//			TouchUtils.clickView(this, orderArrowButton);
+//			OrderDetailActivity startedActivity = (OrderDetailActivity) monitor.waitForActivityWithTimeout(2000);
+//			assertNotNull(startedActivity);
+//			this.sendKeys(KeyEvent.KEYCODE_BACK);
+//			startedActivity.finish();
+//		}
 
 }
