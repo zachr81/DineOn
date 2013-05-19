e0a3226fdf0d3d3ab8774a6c4c85c5a6d15b8872
Mark Rathjen <mrathjen.uw@gmail.com>
2013-05-18 23:35:25 -0700

    Menu items details can now be viewed using parse info

--- DineOnUserApplication/src/uw/cse/dineon/user/restaurant/home/MenuItemDetailFragment.java
+++ DineOnUserApplication/src/uw/cse/dineon/user/restaurant/home/MenuItemDetailFragment.java
@@ -1,7 +1,10 @@
 package uw.cse.dineon.user.restaurant.home;
 
 import uw.cse.dineon.library.MenuItem;
+import uw.cse.dineon.library.RestaurantInfo;
 import uw.cse.dineon.user.R;
+import uw.cse.dineon.user.restaurant.home.SubMenuFragment.MenuItemListListener;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,14 +21,40 @@ public class MenuItemDetailFragment extends Fragment {
 
 	public static final String KEY_MENU = "menu";
 	
+	private MenuItemDetailListener mListener;
+	
+	private MenuItem mMenuItem;
+	
+	private View mView;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_menuitem_detail,
 				container, false);
 		
+		this.mView = view;
 		
 		return view;
+	}
+	
+	@Override 
+	public void onActivityCreated (Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		if (mListener != null)
+			setMenuItem(mListener.getMenuItem());
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		if (activity instanceof MenuItemDetailListener) {
+			mListener = (MenuItemDetailListener) activity;
+			
+		} else {
+			throw new ClassCastException(activity.toString()
+					+ " must implemenet MenuItemDetailFragment.MenuItemDetailListener");
+		}
 	}
 
 	/**
@@ -33,8 +62,33 @@ public class MenuItemDetailFragment extends Fragment {
 	 * @param item String
 	 */
 	public void setMenuItem(MenuItem item) {
-		TextView view = (TextView) getView().findViewById(R.id.label_menu_item_name);
-		view.setText(item.getTitle());
+		this.mMenuItem = item;
+		
+		// set the title
+		TextView title = (TextView) this.mView.findViewById(R.id.label_menu_item_name);
+		title.setText(item.getTitle());
+		
+		// set the description
+		TextView description = (TextView) this.mView.findViewById(R.id.label_menu_item_description);
+		description.setText(item.getDescription());
+		
+		// TODO set the image
+		
+		// TODO set the allergens
+	}
+	
+	/**
+	 * TODO implement.
+	 * @author mrathjen
+	 */
+	public interface MenuItemDetailListener {
+
+		/**
+		 * TODO get the current menu item
+		 * @return MenuItem
+		 */
+		public MenuItem getMenuItem();
+
 	}
 
 }
