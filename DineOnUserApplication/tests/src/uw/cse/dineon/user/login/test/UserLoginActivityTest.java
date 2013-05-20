465e449d727b8334dd90212b5dce373de2d9bcd2
Jordan McNeal <jordanmcneal22@gmail.com>
2013-05-19 19:26:53 -0700

    tests

--- DineOnUserApplication/tests/src/uw/cse/dineon/user/login/test/UserLoginActivityTest.java
+++ DineOnUserApplication/tests/src/uw/cse/dineon/user/login/test/UserLoginActivityTest.java
@@ -1,23 +1,34 @@
 package uw.cse.dineon.user.login.test;
 
+import java.util.Date;
+import java.util.List;
+
 import com.parse.Parse;
 import com.parse.ParseUser;
 
 import uw.cse.dineon.library.DineOnUser;
+import uw.cse.dineon.library.DiningSession;
+import uw.cse.dineon.library.Menu;
+import uw.cse.dineon.library.MenuItem;
+import uw.cse.dineon.library.Order;
+import uw.cse.dineon.library.Restaurant;
+import uw.cse.dineon.library.util.TestUtility;
 import uw.cse.dineon.user.DineOnUserApplication;
+import uw.cse.dineon.user.R;
 import uw.cse.dineon.user.login.UserLoginActivity;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class UserLoginActivityTest extends
 		ActivityInstrumentationTestCase2<UserLoginActivity> {
 
 	private UserLoginActivity mActivity;
-	private EditText mNameText;
-	private ParseUser testUser;
 	private DineOnUser dineOnUser;
-
+	private EditText et_uname, et_passwd;
+	private Instrumentation mInstrumentation;
 	public UserLoginActivityTest() {
 		super(UserLoginActivity.class);
 	}
@@ -28,16 +39,32 @@ public class UserLoginActivityTest extends
 		Parse.initialize(null, "RUWTM02tSuenJPcHGyZ0foyemuL6fjyiIwlMO0Ul", "wvhUoFw5IudTuKIjpfqQoj8dADTT1vJcJHVFKWtK");
 		setActivityInitialTouchMode(false);
 		
-		testUser = ParseUser.logIn("zach", "zach");
-		dineOnUser = new DineOnUser(testUser);
-		DineOnUserApplication.setDineOnUser(dineOnUser);
+		ParseUser user = new ParseUser();
+		user.setUsername("testUser");
+		user.setPassword("12345");;
 		
+		ParseUser restUser = new ParseUser();
+		restUser.setUsername("testRestUser");
+		restUser.setPassword("12345");
+		
+		dineOnUser = new DineOnUser(user);
+		
+		Restaurant rest = new Restaurant(restUser);
+		DiningSession ds = 
+				new DiningSession(10, new Date(), dineOnUser.getUserInfo(), rest.getInfo());
+		
+		List<MenuItem> mi = TestUtility.getFakeMenuItems();
+		Order one = new Order(1, dineOnUser.getUserInfo(), mi);
+		ds.addPendingOrder(one);
+		
+		Menu m = TestUtility.getFakeMenu();
+		m.addNewItem(mi.get(0));
+		rest.getInfo().addMenu(m);
+		this.setActivityInitialTouchMode(false);
+		mInstrumentation = this.getInstrumentation();
 	    Intent addEvent = new Intent();
 	    setActivityIntent(addEvent);
 		mActivity = getActivity();
-		
-		mNameText = (EditText) mActivity.findViewById(uw.cse.dineon.user.R.id.input_login_email);
-		
 	}
 
 	@Override
@@ -46,8 +73,26 @@ public class UserLoginActivityTest extends
 	}
 	
 	public void testOnLogin() {
-		mActivity.onLogin("jordan", "12345");
-		getInstrumentation().waitForIdleSync();
+		et_uname = (EditText) mActivity.findViewById(uw.cse.dineon.user.R.id.input_login_email);
+		et_passwd = (EditText) mActivity.findViewById(R.id.input_login_password);
+		
+		mActivity.runOnUiThread(new Runnable() {
+	          public void run() {
+	              et_uname.setText("");
+	              et_passwd.setText("");
+	              Button loginButton = (Button) mActivity.findViewById(R.id.button_login);
+	              loginButton.performClick();
+	          }
+	      });
+		mInstrumentation.waitForIdleSync();
+		final DineOnUser DU = this.dineOnUser; 
+		mActivity.runOnUiThread(new Runnable() {
+	          public void run() {
+	        	  mActivity.startRestSelectionAct(DU);
+	        	  
+	          }
+	      });
+	    mInstrumentation.waitForIdleSync();
 	}
 	
 }
