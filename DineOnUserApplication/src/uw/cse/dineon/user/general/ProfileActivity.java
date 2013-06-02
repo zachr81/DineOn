391d0176958e5b85e777ae220a49aacad829b9ec
Garrett Lee <garrettlee23@gmail.com>
2013-06-02 16:38:53 -0700

    removes unused, not functioning pay bill button from the profile page

--- DineOnUserApplication/src/uw/cse/dineon/user/general/ProfileActivity.java
+++ DineOnUserApplication/src/uw/cse/dineon/user/general/ProfileActivity.java
@@ -11,6 +11,8 @@ import uw.cse.dineon.library.UserInfo;
 import uw.cse.dineon.user.DineOnUserActivity;
 import uw.cse.dineon.user.DineOnUserApplication;
 import uw.cse.dineon.user.R;
+import uw.cse.dineon.user.bill.CurrentOrderActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -69,13 +71,13 @@ public class ProfileActivity extends DineOnUserActivity implements
 
 		//Sets the necessary onClickListeners for the menu
 		//items with an action layout.
-		final android.view.MenuItem ITEM = menu.findItem(R.id.option_bill);
-		ITEM.setEnabled(true);
-		ITEM.setVisible(true);
-		List<android.view.MenuItem> customActionBarButtons = new ArrayList<android.view.MenuItem>();
-		customActionBarButtons.add(menu.findItem(R.id.option_bill));
-
-		setOnClick(M, customActionBarButtons);
+//		final android.view.MenuItem ITEM = menu.findItem(R.id.option_view_order);
+//		ITEM.setEnabled(true);
+//		ITEM.setVisible(true);
+//		List<android.view.MenuItem> customActionBarButtons = new ArrayList<android.view.MenuItem>();
+//		customActionBarButtons.add(menu.findItem(R.id.option_view_order));
+//
+//		setOnClick(M, customActionBarButtons);
 		return true;
 	}
 	
@@ -94,10 +96,8 @@ public class ProfileActivity extends DineOnUserActivity implements
 		// If checked in
 		if(DineOnUserApplication.getCurrentDiningSession() != null) {
 			disableMenuItem(menu, R.id.option_check_in);
-			enableMenuItem(menu, R.id.option_bill);
 		} else { // If not checked in
 			enableMenuItem(menu, R.id.option_check_in);
-			disableMenuItem(menu, R.id.option_bill);
 		}
 		return true;		
 	}
